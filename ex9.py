93ff9a81733b00a48e3ddec6ada110875ea6793c
juanxto <juanprc1302@gmail.com>
2024-09-11 10:20:53 -0300

    correção

--- ex9.py
+++ ex9.py
@@ -1,6 +1,9 @@
 A = int(input('Digite um valor: '))
 B = int(input('Digite outro valor: '))
+
+C = A
 A = B
-B = A
+B = C
+
 print(f'Valor armazenado na variável A: {A}')
 print(f'Valor armazenado na variável B: {B}')
